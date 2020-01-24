b8903344eabf4676d7efc0dc622fdeb3ed801918
WillDev <codinggilm@gmail.com>
2020-01-24 19:56:23 +0000

    Updates

---
+++ precache-manifest.2ed54a7f78119384d57c4ef10ae4fe35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a4414ea0baa25e255873b2135fe44f",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "f7501b88b10f62ecc134",
+    "url": "/React-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8033bb2d3c2355015011",
+    "url": "/React-App/static/css/main.e3f10e4b.chunk.css"
+  },
+  {
+    "revision": "f7501b88b10f62ecc134",
+    "url": "/React-App/static/js/2.6e759ca9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-App/static/js/2.6e759ca9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8033bb2d3c2355015011",
+    "url": "/React-App/static/js/main.05e0fcb6.chunk.js"
+  },
+  {
+    "revision": "2c2a940b2e0f722a873f",
+    "url": "/React-App/static/js/runtime-main.3bfc035d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
